e0cd717b598baf693624e2bc4c86a7e74e24592e
alexpench <alex.pench@gmail.com>
2019-05-13 16:36:05 +0200

    first commit

---
+++ src/main/java/com/zuul/java/config/JwtConfig.java
@@ -0,0 +1,37 @@
+package com.zuul.java.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class JwtConfig {
+    @Value("${security.jwt.uri:/auth/**}")
+    private String Uri;
+
+    @Value("${security.jwt.header:Authorization}")
+    private String header;
+
+    @Value("${security.jwt.prefix:Bearer }")
+    private String prefix;
+
+    @Value("${security.jwt.expiration:#{24*60*60}}")
+    private int expiration;
+
+    @Value("${security.jwt.secret:JwtSecretKey}")
+    private String secret;
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public String getUri() {
+        return Uri;
+    }
+}
