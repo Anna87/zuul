2d328eba3aef2c101d3a89ebfe849d3d0716ff09
alexpench <alex.pench@gmail.com>
2019-05-30 18:40:40 +0200

    error handle

--- src/main/java/com/zuul/java/filter/UserHeadersFilter.java
+++ src/main/java/com/zuul/java/filter/UserHeadersFilter.java
@@ -5,6 +5,7 @@ import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import com.zuul.java.config.JwtConfig;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,12 +47,20 @@ public class UserHeadersFilter extends ZuulFilter {
         if(nonNull(rawPrefix)) {
             final String token = rawPrefix.substring(jwtConfig.getPrefix().length());
 
-            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(jwtConfig.getSecret().getBytes()).parseClaimsJws(token);
-            List<String> roles = claimsJws.getBody().get("authorities", List.class);
-            String username = claimsJws.getBody().getSubject();
+            try{
+                Jws<Claims> claimsJws = Jwts.parser().setSigningKey(jwtConfig.getSecret().getBytes()).parseClaimsJws(token);
+                List<String> roles = claimsJws.getBody().get("authorities", List.class);
+                String username = claimsJws.getBody().getSubject();
 
-            currentContext.addZuulRequestHeader("username", username);
-            currentContext.addZuulRequestHeader("authorities", String.join(", ", roles));
+                currentContext.addZuulRequestHeader("username", username);
+                currentContext.addZuulRequestHeader("authorities", String.join(", ", roles));
+            }
+            catch (ExpiredJwtException e){
+                currentContext.setSendZuulResponse(false);
+                currentContext.setResponseStatusCode(401);
+                currentContext.setResponseBody("Token has expired");
+            }
+
         }
 
         return null;
