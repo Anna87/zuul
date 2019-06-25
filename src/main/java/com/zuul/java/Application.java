c859889323f619dfe290933f9b468ede33371530
alexpench <alex.pench@gmail.com>
2019-06-25 13:24:39 +0200

    refactoring

--- src/main/java/com/zuul/java/Application.java
+++ src/main/java/com/zuul/java/Application.java
@@ -13,6 +13,7 @@ import org.springframework.web.filter.CorsFilter;
 @EnableEurekaClient
 @EnableZuulProxy
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
